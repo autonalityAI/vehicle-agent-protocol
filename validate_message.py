359442fb480e70dc89e04023bac882b6a9ccdeab
schac12 <69211769+schac12@users.noreply.github.com>
2025-05-02 11:54:19 +0200

    Create validate_message.py

---
+++ validate_message.py
@@ -0,0 +1,30 @@
+import json
+import sys
+from jsonschema import validate, ValidationError, SchemaError
+
+# Rutas por defecto
+SCHEMA_FILE = "schema/vas-message-schema.json"
+
+def main(json_file_path):
+    try:
+        with open(SCHEMA_FILE, 'r') as schema_file:
+            schema = json.load(schema_file)
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+
+        validate(instance=data, schema=schema)
+        print(f"✅ {json_file_path} is valid ✅")
+    except FileNotFoundError:
+        print("❌ File not found. Check the path.")
+    except ValidationError as ve:
+        print(f"❌ Validation error: {ve.message}")
+    except SchemaError as se:
+        print(f"❌ Schema error: {se.message}")
+    except Exception as e:
+        print(f"❌ Unexpected error: {str(e)}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python validate_message.py <json_file_to_validate>")
+    else:
+        main(sys.argv[1])
